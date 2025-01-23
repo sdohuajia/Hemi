55e51f806742bf065d568b3f25ef7641d20623b6
sdohuajia <63113882+sdohuajia@users.noreply.github.com>
2025-01-23 17:29:52 +0800

    Update Hemi.sh

--- Hemi.sh
+++ Hemi.sh
@@ -91,9 +91,9 @@ generate_key() {
     check_go_version
     install_pm2
 
-    URL="https://github.com/hemilabs/heminetwork/releases/download/v0.10.0/heminetwork_v0.10.0_linux_amd64.tar.gz"
-    FILENAME="heminetwork_v0.10.0_linux_amd64.tar.gz"
-    DIRECTORY="/root/heminetwork_v0.10.0_linux_amd64"
+    URL="https://github.com/hemilabs/heminetwork/releases/download/v0.11.1/heminetwork_v0.11.1_linux_amd64.tar.gz"
+    FILENAME="heminetwork_v0.11.1_linux_amd64.tar.gz"
+    DIRECTORY="/root/heminetwork_v0.11.1_linux_amd64"
     OUTPUT_FILE="$HOME/popm-address.json"
 
     echo "正在下载 $FILENAME..."
@@ -143,7 +143,7 @@ generate_key() {
 
 # 运行节点函数
 run_node() {
-    DIRECTORY="$HOME/heminetwork_v0.10.0_linux_amd64"
+    DIRECTORY="$HOME/heminetwork_v0.11.1_linux_amd64"
 
     echo "进入目录 $DIRECTORY..."
     cd "$DIRECTORY" || { echo "目录 $DIRECTORY 不存在。"; exit 1; }
@@ -270,11 +270,6 @@ backup_address_json() {
 
 # 查看日志函数
 view_logs() {
-    DIRECTORY="heminetwork_v0.10.0_linux_amd64"
-
-    echo "进入目录 $DIRECTORY..."
-    cd "$HOME/$DIRECTORY" || { echo "目录 $DIRECTORY 不存在。"; exit 1; }
-
     echo "查看 pm2 日志..."
     pm2 logs popmd
 
